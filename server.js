46568ee9c28113c012aa28206bb135289490bdc8
Eric Kleinsorgen <emk@wopr.kdc.jadek.com>
2021-02-01 15:26:07 -0500

    make some changes

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const express = require('express');
 // Constants
 const PORT = process.env.PORT || "8080";
 const HOST = process.env.HOST || "0.0.0.0";
-const VERSION = process.env.VERSION || "0.0";
+const VERSION = process.env.VERSION || "1.0";
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello RBC !!!!!!!  VERSION ${VERSION} ');
+  res.send('Hello Eric !!!!!!!  VERSION ${VERSION} ');
 });
 
 app.listen(PORT, HOST);
