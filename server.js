a84057f91df827877f6bec28a7dc8b7b497779ab
Eric Kleinsorgen <emk@wopr.kdc.jadek.com>
2021-02-01 15:41:22 -0500

    fixed node app

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const express = require('express');
 
 
 // Constants
-const PORT = process.env.PORT || "8080";
-const HOST = process.env.HOST || "0.0.0.0";
-const VERSION = process.env.VERSION || "1.0";
+const port = process.env.PORT || "8080";
+const host = process.env.HOST || "0.0.0.0";
+const version = process.env.VERSION || "1.0";
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Eric !!!!!!!  VERSION ${VERSION} ');
+  res.send('Hello Eric !!!!!!!  VERSION ${version} ');
 });
 
 app.listen(PORT, HOST);
-console.log('Running on http://${HOST}:${PORT}');
+console.log('Running on http://${host}:${port}');
